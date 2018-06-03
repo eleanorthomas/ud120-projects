b99e2406b10bbda6ae05883ca786b7cb9ac7a39b
Eleanor Thomas <eleanor.thomas.res@gmail.com>
2018-06-02 20:54:36 -0500

    Tried AdaBoost, RandomForest, and kNN for choose-your-own

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -24,19 +24,25 @@ plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
 plt.legend()
 plt.xlabel("bumpiness")
 plt.ylabel("grade")
-plt.show()
+# plt.show()
 ################################################################################
 
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
+clf = KNeighborsClassifier(n_neighbors=8)
+# clf = RandomForestClassifier(criterion='entropy')
+# clf = AdaBoostClassifier(learning_rate=1.5)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+acc = accuracy_score(pred, labels_test)
 
-
-
-
-
-
+print acc
 
 try:
     prettyPicture(clf, features_test, labels_test)
